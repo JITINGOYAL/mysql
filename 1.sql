3c306502c58b6414f9129ca7375c42826eb155c0
Jitin Goyal <jitingoyal4@gmail.com>
2020-09-22 23:16:06 +0530

    Initial commit

---
+++ 1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE countries(
+ country_code varchar(50) primary key,
+ country_name varchar(50),
+ continent varchar(50),
+ region varchar(50),
+ independence_year smallint,
+ local_name varchar(50),
+ gov_form varchar(50),
+ capital varchar(50)
+);
